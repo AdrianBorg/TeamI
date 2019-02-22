9f76d3858be2bcaae380191033b75f06905a3589
Ifigenia Temesio <1104159h@student.gla.ac.uk>
2019-02-22 18:48:38 +0000

    updated css file with headers and button

---
+++ static/js/baseScript.js
@@ -0,0 +1,24 @@
+
+function home() {
+    window.location = "http://localhost:8000/trimit/"
+}
+
+function hairdresserSignup(){
+    window.location = "http://localhost:8000/trimit/"
+}
+
+function userSignup(){
+    window.location = "http://localhost:8000/trimit/"
+}
+
+function login(){
+    window.location = "http://localhost:8000/trimit/"
+}
+
+function contact(){
+    window.location = "http://localhost:8000/trimit/"
+}
+
+function about(){
+    window.location = "http://localhost:8000/trimit/"
+}
