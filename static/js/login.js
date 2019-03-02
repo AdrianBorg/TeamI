5cf01152ec8a97fb85030988d837fc2056a7cc83
Adrian Borg <borgadrian@yahoo.co.uk>
2019-03-02 00:58:42 +0000

    created modal, with 2 tabs that switch

--- static/js/login.js
+++ static/js/login.js
@@ -7,12 +7,50 @@ var btn = document.getElementById("myBtn");
 // Get the <span> element that closes the modal
 var cls = document.getElementsByClassName("close")[0];
 
+// Get the modal tab elements
+var loginTab = document.getElementById("login-tab-title");
+var signupTab = document.getElementById("signup-tab-title");
+var activeTab = loginTab;
+
 // When the user clicks on the button, open the modal
 btn.onclick = function() {
-  modal.style.display = "block";
+    modal.style.display = "block";
 }
 
 // When the user clicks on <span> (x), close the modal
 cls.onclick = function() {
-  modal.style.display = "none";
+    modal.style.display = "none";
+}
+
+// When the user picks a tab, do something
+loginTab.onclick = function() {
+    if (activeTab != loginTab) {
+        switch2login();
+    }
+}
+
+signupTab.onclick = function() {
+    if (activeTab != signupTab) {
+        switch2signup();
+    }
+}
+
+function switch2login() {
+    $('#login-tab-title').css("background-color", '#f4eaff');
+    $('#login-tab-title').css("color", '#a81d73');
+    $('#signup-tab-title').css("background-color", '#a81d73');
+    $('#signup-tab-title').css("color", '#ffffff');
+    activeTab = loginTab;
+    $('#signup-form').css("display", "none");
+    $('#login-form').css("display", "block");
+}
+
+function switch2signup() {
+    $('#signup-tab-title').css("background-color", '#f4eaff');
+    $('#signup-tab-title').css("color", '#a81d73');
+    $('#login-tab-title').css("background-color", '#a81d73');
+    $('#login-tab-title').css("color", '#ffffff');
+    activeTab = signupTab
+    $('#login-form').css("display", "none");
+    $('#signup-form').css("display", "block");
 }
