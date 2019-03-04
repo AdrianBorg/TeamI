5c1f02e04925b5391d44cab661631c1d43f9d45a
Adrian Borg <borgadrian@yahoo.co.uk>
2019-03-04 01:21:49 +0000

    formatted forms, added flags for countrywidget, added form css

--- static/js/register.js
+++ static/js/register.js
@@ -6,3 +6,8 @@ $(document).ready(function () {
         }, 3000);
     }
 })
+
+$('#country-field').on('change', function () {
+    var url = $('#head').attr('data-static-images') + 'flags/' + $('#country-field').val().toLowerCase() + '.gif';
+    $('.country-select-flag').attr('src', url);
+})
--- trimit/forms.py
+++ trimit/forms.py
@@ -1,6 +1,7 @@
 from django import forms
 from django.contrib.auth.models import User
 from trimit.models import UserProfile, Page
+from django_countries.widgets import CountrySelectWidget
 
 
 class UserRegisterForm(forms.ModelForm):
@@ -24,10 +25,14 @@ class HairdresserPageForm(forms.ModelForm):
         max_length=300,
         widget=forms.Textarea,
     )
+
     class Meta:
         model = Page
         fields = ('name', 'flat_number', 'street_address', 'city', 'postcode', 'country', 'opening_times',
                   'contact_number', 'profile_picture', 'webpage', 'instagram',)
+        widgets = {'country': CountrySelectWidget(
+            layout='<div class="country-widget">{widget}<img class="country-select-flag" id="{flag_id}" style="margin: 6px 0 0 4px" src="{country.flag}"></div>'
+        )}
 
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
