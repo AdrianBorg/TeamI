db26dacf1be4bf660b5bebc56b8622dde6ab3229
Adrian Borg <borgadrian@yahoo.co.uk>
2019-03-16 02:18:40 +0000

    adding static root in settings

--- TeamI/settings.py
+++ TeamI/settings.py
@@ -126,7 +126,7 @@ USE_TZ = False # to ignore the RuntimeWarning:
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
-
+STATIC_ROOT = STATIC_DIR
 STATIC_URL = '/static/'
 
 STATICFILES_DIRS = [STATIC_DIR, ]
