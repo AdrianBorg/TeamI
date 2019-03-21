1752ccc573df9120cb560146981b32b7a4628691
Adrian Borg <borgadrian@yahoo.co.uk>
2019-03-21 22:47:26 +0000

    fixing bug in hairpage

--- trimit/views.py
+++ trimit/views.py
@@ -289,7 +289,7 @@ def ajax_user_login(request):
 def hairdresser_page(request, hairdresser_slug):
     user = request.user #UserProfile.objects.get(id=request.user.id)
     hairdresser = Page.objects.get(slug=hairdresser_slug)
-    if request.user.is_anonymous:
+    if not request.user.is_anonymous:
         has_user_page = UserProfile.objects.filter(user=request.user).exists()
     review_list = Review.objects.filter(page__slug=hairdresser.slug)
     a = (hairdresser.user==request.user)
